f76d2434363d3355ae8d207beaa7474a1e18cdaa
Mark Bann <MarkBann44@gmail.com>
2015-08-04 16:35:12 -0400

    'Merge' added isOpen to JSON object and pizzaservlet

--- src/PizzaServlet.java
+++ src/PizzaServlet.java
@@ -1,5 +1,6 @@
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -18,6 +19,24 @@ import org.json.JSONException;
 public class PizzaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	public static final String stationAddress = null;
+	public static HashMap<String, String> stationAddresses = null;
+	static{
+		stationAddresses = new HashMap<>();
+		stationAddresses.put("Times Square", "42.333665,-83.052125");
+		stationAddresses.put("Grand Circus Park", "42.335855,-83.050510");
+		stationAddresses.put("Broadway", "42.335437,-83.048204");
+		stationAddresses.put("Cadillac Center", "42.333766,-83.046394");
+		stationAddresses.put("Greektown", "42.334909,-83.042425");
+		stationAddresses.put("Bricktown", "42.333282,-83.041168");
+		stationAddresses.put("Renaissance Center", "42.330147,-83.040208");
+		stationAddresses.put("Millender Center", "42.330250,-83.041995");
+		stationAddresses.put("Financial District", "42.328687,-83.046697");
+		stationAddresses.put("Joe Louis Arena", "42.325211,-83.051361");
+		stationAddresses.put("Cobo Center", "42.327938,-83.049715");
+		stationAddresses.put("Fort Cass", "42.329550,-83.051051");
+		stationAddresses.put("Michigan Ave.", "42.331344,-83.052163");
+	
+	}
 
 	/**
 	 * @see HttpServlet#HttpServlet()
@@ -33,34 +52,29 @@ public class PizzaServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		System.out.println("do get method");
 		
-		response.getWriter().write("do get method");
 	}
-
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String keyword = request.getParameter("keyword");
-		keyword = keyword.trim();
-		keyword = keyword.replaceAll("\\s+", "%20");
 		String stationID = request.getParameter("stationID");
-		request.setAttribute("stationName", stationID);
-		HashMap<String, String> searchAddress = getStationAddress();		
-		String stationAddress = searchAddress.get(stationID);
+		keyword = insertHTMLSpaces(keyword);
+		request.setAttribute("stationName", stationID); //Why is this line necessary?
+		String stationAddress = getStationAddress(stationID);		
 		String stationLat = stationAddress.substring(0, 9);
 		String stationLng = stationAddress.substring(10);
 		String formattedCoords = stationLat + ", " + stationLng;
 		request.setAttribute("stationCoords", formattedCoords);
-		System.out.println(formattedCoords);
-		System.out.println(stationAddress);
-		System.out.println(keyword);
+//		System.out.println(formattedCoords);
+//		System.out.println(stationAddress);
+//		System.out.println(keyword);
 		ArrayList<Location> locations; 
 		try {
 			String googlePlaces = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=" + keyword + "&location=" + stationAddress + "&radius=300&key=AIzaSyAxC5yolZSd5G-UiQc70px_8cX0T09mqNs";
-			locations = JsonObjectArray.getLocationArray(googlePlaces);
 			System.out.println(googlePlaces);
+			locations = JsonObjectArray.getLocationArray(googlePlaces);
 			request.setAttribute("location", locations);
 		} catch (JSONException e) {
 			e.printStackTrace();
@@ -68,22 +82,14 @@ public class PizzaServlet extends HttpServlet {
 		getServletContext().getRequestDispatcher("/results.jsp").forward(request, response);
 	}
 
-	private HashMap<String, String> getStationAddress() {
-		HashMap<String, String> stationAddresses = new HashMap<>();
-		stationAddresses.put("Times Square", "42.333665,-83.052125");
-		stationAddresses.put("Grand Circus Park", "42.335855,-83.050510");
-		stationAddresses.put("Broadway", "42.335437,-83.048204");
-		stationAddresses.put("Cadillac Center", "42.333766,-83.046394");
-		stationAddresses.put("Greektown", "42.334909,-83.042425");
-		stationAddresses.put("Bricktown", "42.333282,-83.041168");
-		stationAddresses.put("Renaissance Center", "42.330147,-83.040208");
-		stationAddresses.put("Millender Center", "42.330250,-83.041995");
-		stationAddresses.put("Financial District", "42.328687,-83.046697");
-		stationAddresses.put("Joe Louis Arena", "42.325211,-83.051361");
-		stationAddresses.put("Cobo Center", "42.327938,-83.049715");
-		stationAddresses.put("Fort Cass", "42.329550,-83.051051");
-		stationAddresses.put("Michigan Ave.", "42.331344,-83.052163");
-		return stationAddresses;
+	static String insertHTMLSpaces(String keyword) {
+		keyword = keyword.trim();
+		keyword = keyword.replaceAll("\\s+", "%20");
+		return keyword;
+	}
+
+	static String getStationAddress(String stationId) {
+			return stationAddresses.get(stationId);
 	}
 
 }
